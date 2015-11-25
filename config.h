fcbbbb0da293965ff605c8372460c0cf50304e71
unknown <rj150@hotmail.com>
2015-11-25 11:19:06 -0500

    conflicting VCR_SUPPORT #define and #undef if _WIN32

--- config.h
+++ config.h
@@ -2,7 +2,19 @@
 #define CONFIG_H
 
 #undef WITH_HOME
+
+/*
+ * 2015.11.25 cxd4
+ *
+ * To do:  Win32 port of Mupen64 unconditionally calls VCR functions.
+ * Therefore the #undef VCR breaks Win32 builds, while Win32 make defines it.
+ *
+ * Probably should look for a way to detach VCR dependency from Win32 port.
+ */
+#ifndef _WIN32
 #undef VCR_SUPPORT
+#endif
+
 #define GTK2_SUPPORT 1
 
 #endif /* CONFIG_H */
